c2eeb2435928100da68ff7458cc19294ac5599a4
Wesley <wesleycamilo@gmail.com>
2022-11-08 14:00:56 -0300

    Initial Commit

---
+++ Pokédex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokédex
+//
+//  Created by Wesley Camilo on 08/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
